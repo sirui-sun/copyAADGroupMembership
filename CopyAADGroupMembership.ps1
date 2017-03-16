cdddfbb5e06382f7c7ded45401d572c5b852a602
Sirui Sun <sirsu@microsoft.com>
2017-03-15 18:34:57 -0700

    add error handling, formatting changes

--- CopyAADGroupMembership.ps1
+++ CopyAADGroupMembership.ps1
@@ -5,39 +5,48 @@
 "This script takes a SOURCE group and a TARGET group."
 "The TARGET group's membership is made to exactly match the SOURCE group's."
 "The SOURCE group's membership is unchanged."
-$SourceGroupName = Read-Host -Prompt "Input the SOURCE group name"
-$TargetGroupName = Read-Host -Prompt "Input the TARGET group name"
 
+Try {
+    # Connect with Credential Object
+    $AzureAdCred = Get-Credential | out-null
+    Connect-AzureAD -Credential $AzureAdCred
+} Catch {
+    # Print error and exit if sign-in failed
+    echo $_.Exception.GetType().FullName, $_.Exception.Message
+    break
+}
+
+# Ask for SOURCE and TARGET group display name
+$SourceGroupName = Read-Host -Prompt "Input the SOURCE group display name"
+$TargetGroupName = Read-Host -Prompt "Input the TARGET group display name"
+
+# Counters used for the summary message when script is finished
 $membersAdded = 0
 $membersRemoved = 0
-
-# Connect with Credential Object
-$AzureAdCred = Get-Credential
-Connect-AzureAD -Credential $AzureAdCred | out-null
 
 # Get groups and their members
 $sourceGroup = Get-AzureADGroup -Filter "DisplayName eq '$SourceGroupName'"
 $targetGroup = Get-AzureADGroup -Filter "DisplayName eq '$TargetGroupName'"
-$source_members = Get-AzureADGroupMember -ObjectId $sourceGroup.ObjectId
-$target_members = Get-AzureADGroupMember -ObjectId $targetGroup.ObjectId
+$sourceMembers = Get-AzureADGroupMember -ObjectId $sourceGroup.ObjectId
+$targetMembers = Get-AzureADGroupMember -ObjectId $targetGroup.ObjectId
 
 # Add all members from SOURCE to TARGET
-foreach ($member in $source_members) {
-    If ($target_members -notcontains $member) {    
+foreach ($member in $sourceMembers) {
+    If ($targetMembers -notcontains $member) {    
         Add-AzureADGroupMember -ObjectId $targetGroup.ObjectId -RefObjectId $member.ObjectId
         $membersAdded += 1
     }
 }
 
 # Remove all members from TARGET who are not in SOURCE
-foreach ($member in $target_members) {
-    If ($source_members -notcontains $member) {
+foreach ($member in $targetMembers) {
+    If ($sourceMembers -notcontains $member) {
         Remove-AzureADGroupMember -ObjectId $targetGroup.ObjectId -MemberId $member.ObjectId
         $membersRemoved += 1
     }
 }
 
 ""
-"SUCCESS: Operation completed."
+"Operation completed."
 "$membersAdded members added to TARGET group."
 "$membersRemoved members removed from TARGET group."
